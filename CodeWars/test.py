d28c7ed706a856477f57119420610a6613e20bb9
MuhammedMiiRanna <merali.med@gmail.com>
2023-09-09 15:44:17 +0100

    Update test.py

--- CodeWars/test.py
+++ CodeWars/test.py
@@ -1,4 +1,24 @@
 
+
+
+def smallest(n):
+    s = str(n)
+    min1, from1, to1 = n, 0, 0
+    for i in range(len(s)):
+        removed = s[:i] + s[i+1:]
+        for j in range(len(removed)+1):
+            num = int(removed[:j] + s[i] + removed[j:])
+            if (num < min1):
+                min1, from1, to1 = num, i, j
+    return [min1, from1, to1]
+
+
+n = 1000000
+n = 261235
+n = 209917
+
+res = smallest(n)
+
 # import signal
 # TIMEOUT = 5  # number of seconds your want for timeout
 
